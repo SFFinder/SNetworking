fada719f352962fb13a26252abc0ddb6d0f70883
Aaron <airfree_shaoxu@163.com>
2022-02-23 10:44:35 +0800

    shaoxu
    first commit

---
+++ XNetworking/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  XNetworking
+//
+//  Created by 11 on 2022/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XNetworking/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  XNetworking
+//
+//  Created by 11 on 2022/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
